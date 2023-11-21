6288f8ccab0c50a80979d9f4ee3bf99c3e5e71c8
Sapkota Sushma <a01363028@unet.univie.ac.at>
2023-11-21 23:05:46 +0100

    feat: image forwarded to face recognition.

--- collector/service.py
+++ collector/service.py
@@ -16,6 +16,7 @@ load_dotenv()
 logger = logging.getLogger(__name__)
 
 image_analysis_url = os.getenv("IMAGE_ANALYSIS_URL")
+face_recognition_url = os.getenv("FACE_RECOGNITION_URL")
 
 origins = ["*"]
 
@@ -29,6 +30,7 @@ app.add_middleware(
 
 collector_frames_list = []
 image_analysis_frames_list = []
+face_recognition_frames_list = []
 
 
 def updated_frame(frame_dict: Dict) -> Dict:
@@ -45,38 +47,53 @@ def updated_frame(frame_dict: Dict) -> Dict:
     return filtered_fields
 
 
-def forward_to_image_analysis(body_dict):
+def forward_request(url: str, json_data: Dict) -> httpx.Response:
     try:
         timestamp = datetime.today().isoformat()
-        body_dict["timestamp"] = timestamp
-        response = httpx.post(image_analysis_url + "/frame", json=body_dict)
+        json_data["timestamp"] = timestamp
+        response = httpx.post(url, json=json_data)
         response.raise_for_status()
         return response
-
     except httpx.HTTPError as e:
-        logger.error("Image analysis service error: %s", e.response.text)
+        logger.error(f"Service error: {e.response.text}")
         raise HTTPException(status_code=e.response.status_code,
-                            detail="Image analysis service error")
-
+                            detail=f"Service error: {url}")
     except Exception as e:
-        logger.error("Error during frame forwarding: %s", str(e))
-        return None  # Return None in case of an exception
+        logger.error(f"Error during request forwarding: {str(e)}")
+        return None
+
+
+def forward_to_image_analysis(body_dict):
+    return forward_request(image_analysis_url + "/frame", body_dict)
+
+
+def forward_to_face_recognition(json_data):
+    updated_json_data = updated_frame(json_data)
+    return forward_request(face_recognition_url + "/frame", updated_json_data)
 
 
 @app.post("/frame", status_code=status.HTTP_201_CREATED)
 def create_frames(payload: CollectorFrame):
-
     frame_dict = payload.model_dump()
     updated_payload = updated_frame(frame_dict)
 
     collector_frames_list.append(frame_dict)
 
     response = forward_to_image_analysis(updated_payload)
-    print(response.json())
 
-    if response.status_code == status.HTTP_200_OK:
+    # Ensure the response object exists, is not None, and has an okay status code.
+    if response and response.status_code == status.HTTP_200_OK:
         image_analysis_frames_list.append(response.json())
-        return {"message": "Frame added successfully"}
+
+        face_recognition_response = forward_to_face_recognition(
+            response.json())
+
+        if face_recognition_response and face_recognition_response.status_code == status.HTTP_200_OK:
+            face_recognition_frames_list.append(
+                face_recognition_response.json())
+            return {"message": "Frame added successfully"}
+        else:
+            return {"message": "Frame forwarding to face recognition failed"}
     else:
         return {"message": "Frame forwarding failed"}
 
@@ -89,3 +106,8 @@ def get_frames():
 @app.get("/imageAnalysisFrames")
 def get_frames():
     return image_analysis_frames_list
+
+
+@app.get("/faceRecognitionFrames")
+def get_frames():
+    return face_recognition_frames_list
