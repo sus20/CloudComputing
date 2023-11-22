8ec3d788a24f898a179e1961c2f0044ce97cabec
Sapkota Sushma <a01363028@unet.univie.ac.at>
2023-11-23 00:27:14 +0100

    fix: toMakeConnectionToSectionAndMongoDB EXTERNAL_MONGODB_URL has been changed , logger added in service.py

--- collector/service.py
+++ collector/service.py
@@ -15,6 +15,9 @@ app = FastAPI()
 load_dotenv()
 logger = logging.getLogger(__name__)
 
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
 image_analysis_url = os.getenv("IMAGE_ANALYSIS_URL")
 face_recognition_url = os.getenv("FACE_RECOGNITION_URL")
 alert_url = os.getenv("ALERT_URL")
@@ -30,9 +33,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-collector_frames_list = []
-image_analysis_frames_list = []
-face_recognition_frames_list = []
+
+def configure_logging():
+    logging.basicConfig(level=logging.INFO)
+
+
+def forward_request(url: str, json_data: Dict) -> httpx.Response:
+    try:
+        timestamp = datetime.today().isoformat()
+        json_data["timestamp"] = timestamp
+        response = httpx.post(url, json=json_data)
+        response.raise_for_status()
+        logger.info(f"Request successfully forwarded to {url}")
+        return response
+
+    except httpx.HTTPError as e:
+        logger.error(f"Service error: {e.response.text}")
+        raise HTTPException(status_code=e.response.status_code,
+                            detail=f"Service error: {url}")
+    except Exception as e:
+        logger.error(f"Error during request forwarding: {str(e)}")
+        return None
 
 
 def updated_frame(frame_dict: Dict) -> Dict:
@@ -49,24 +70,9 @@ def updated_frame(frame_dict: Dict) -> Dict:
     return filtered_fields
 
 
-def forward_request(url: str, json_data: Dict) -> httpx.Response:
-    try:
-        timestamp = datetime.today().isoformat()
-        json_data["timestamp"] = timestamp
-        response = httpx.post(url, json=json_data)
-        response.raise_for_status()
-        return response
-    except httpx.HTTPError as e:
-        logger.error(f"Service error: {e.response.text}")
-        raise HTTPException(status_code=e.response.status_code,
-                            detail=f"Service error: {url}")
-    except Exception as e:
-        logger.error(f"Error during request forwarding: {str(e)}")
-        return None
-
-
-def forward_to_image_analysis(body_dict):
-    return forward_request(image_analysis_url + "/frame", body_dict)
+def forward_to_image_analysis(payload):
+    updated_payload = updated_frame(payload)
+    return forward_request(image_analysis_url + "/frame", updated_payload)
 
 
 def forward_to_face_recognition(json_dict):
@@ -86,14 +92,11 @@ def forward_to_section(json_dict):
 def create_frames(payload: CollectorFrame):
 
     frame_dict = payload.model_dump()
-    updated_payload = updated_frame(frame_dict)
-    collector_frames_list.append(frame_dict)
-    image_res = forward_to_image_analysis(updated_payload)
+    image_res = forward_to_image_analysis(frame_dict)
 
     if not image_res or image_res.status_code != status.HTTP_200_OK:
         return {"message": "Frame forwarding to image analysis failed"}
 
-    image_analysis_frames_list.append(image_res.json())
     fr_res = forward_to_face_recognition(image_res.json())
 
     if not fr_res or fr_res.status_code != status.HTTP_200_OK:
@@ -105,7 +108,6 @@ def create_frames(payload: CollectorFrame):
             return {"message": "Frame forwarding to section service failed"}
 
     fr_res_data = fr_res.json()
-    face_recognition_frames_list.append(fr_res_data)
 
     if "known-persons" in fr_res_data:
         alert_response = forward_to_alert(fr_res_data)
@@ -116,18 +118,3 @@ def create_frames(payload: CollectorFrame):
             return {"message": "Forwarding to alert service failed"}
 
     return {"message": "Frame forwarding to face recognition failed"}
-
-
-@app.get("/frames", response_model=List[CollectorFrame])
-def get_frames():
-    return collector_frames_list
-
-
-@app.get("/imageAnalysisFrames")
-def get_frames():
-    return image_analysis_frames_list
-
-
-@app.get("/faceRecognitionFrames")
-def get_frames():
-    return face_recognition_frames_list
