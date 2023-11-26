aa485351a5fa93a1d7b1097d5be7d2a7f5fc38e0
Sapkota Sushma <a01363028@unet.univie.ac.at>
2023-11-26 17:06:31 +0100

    kubernetes individual pods, deployment and services for all microservices.

---
+++ kubernetes/run_kubectl.bat
@@ -0,0 +1,10 @@
+kubectl apply -f configmap.yaml
+kubectl apply -f sectiondb-deploy-service.yaml
+kubectl apply -f alert-deploy-service.yaml
+kubectl apply -f camera-deploy-service.yaml
+kubectl apply -f collector-deploy-service.yaml
+kubectl apply -f face-recognition-deploy-service.yaml
+kubectl apply -f image-analysis-deploy-service.yaml
+kubectl apply -f mongoexpress-deploy-service.yaml
+kubectl apply -f section-deploy-service.yaml
+
